9db2e617ed3dea4bed14d4f03547f9fd2c3bae7d
junhee4613 <junhee000123@naver.com>
2023-05-03 15:40:52 +0900

    인벤토리 제작 사이트 저장

---
+++ studyJunhee/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    //인벤토리 제작 사이트: https://ansohxxn.github.io/unity%20lesson%203/ch5-2/#%EC%9D%B8%EB%B2%A4%ED%86%A0%EB%A6%AC-ui-%EA%B5%AC%EC%A1%B0
+}
